a01ebfdde03a7e20e44f7a8e19492f529df5b192
Eloy <eesteban@iesrafaelalberti.es>
2024-01-28 00:43:40 +0100

    Implementación del Bombo en Jugar

--- Bingo.kt
+++ Bingo.kt
@@ -1,3 +1,45 @@
+import kotlin.random.Random
+
+class Bombo(private val bombo: MutableList<Int> = (1..90).toMutableList()) {
+    private val numerosGenerados: MutableList<Int> = mutableListOf()
+    private var bingoCantado = false
+
+    fun bombo() {
+        while (bombo.isNotEmpty() && !bingoCantado) {
+            val numero = generarNumeroAleatorio()
+            numerosGenerados.add(numero)
+            println("Número generado: $numero")
+
+            // Verifica si se ha cantado bingo (condición ficticia)
+            if (verificarBingo(numerosGenerados)) {
+                bingoCantado = true
+                println("¡Bingo!")
+            }
+
+            // Espera 2 segundos antes de generar el próximo número
+            Thread.sleep(2000)
+        }
+
+        // Imprime la lista de números generados
+        println("Números generados: $numerosGenerados")
+    }
+    // Si se acaban los números del bombo arroja un mensaje
+    private fun generarNumeroAleatorio(): Int {
+        if (bombo.isEmpty()) {
+            throw IllegalStateException("Bombo vacío, todos los números han sido generados.")
+        }
+
+        val indiceAleatorio = Random.nextInt(bombo.size)
+        val numero = bombo.removeAt(indiceAleatorio)
+
+        return numero
+    }
+
+    private fun verificarBingo(numerosGenerados: List<Int>): Boolean {
+        // Lógica para verificar si se ha cantado bingo
+        return false
+    }
+}
 class Menu {
     fun mostrarMenu() {
         println("1. Jugar")
@@ -8,7 +50,8 @@ class Menu {
 
     fun jugar() {
         println("¡Bienvenido a la función Jugar!")
-        // Falta funcionalidades jugar
+        val simulador = Bombo()
+        simulador.bombo()
     }
 
     fun mostrarInstrucciones() {
@@ -16,6 +59,8 @@ class Menu {
         // Faltan Instrucciones
     }
 }
+
+
 
 fun main() {
     val menu = Menu()
