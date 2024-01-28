70f16f8e76bb4aa321f0796a1a304b35acfd6f0e
Eloy <eesteban@iesrafaelalberti.es>
2024-01-28 14:34:09 +0100

    Implementación del Carton Aleatorio

--- Bingo.kt
+++ Bingo.kt
@@ -1,5 +1,7 @@
 import kotlin.random.Random
 
+
+//Clase Bombo
 class Bombo(private val bombo: MutableList<Int> = (1..90).toMutableList()) {
     private val numerosGenerados: MutableList<Int> = mutableListOf()
     private var bingoCantado = false
@@ -40,6 +42,46 @@ class Bombo(private val bombo: MutableList<Int> = (1..90).toMutableList()) {
         return false
     }
 }
+
+
+//Clase Carton
+class CartonBingoAleatorio {
+    private val filas = 3
+    private val columnas = 5
+    private val carton = Array(filas) { IntArray(columnas) }
+
+    init {
+        generarCartonBingoAleatorio()
+    }
+
+    private fun generarCartonBingoAleatorio() {
+        val numerosDisponibles = mutableListOf<Int>()
+
+        for (i in 1..90) {
+            numerosDisponibles.add(i)
+        }
+
+        for (i in 0 until filas) {
+            for (j in 0 until columnas) {
+                val numeroAleatorio = numerosDisponibles.random()
+                carton[i][j] = numeroAleatorio
+                numerosDisponibles.remove(numeroAleatorio)
+            }
+        }
+    }
+
+    fun mostrarCarton() {
+        println("Cartón de Bingo:")
+        for (i in 0 until filas) {
+            for (j in 0 until columnas) {
+                print("${carton[i][j]}\t")
+            }
+            println()
+        }
+    }
+}
+
+//Clase Menu
 class Menu {
     fun mostrarMenu() {
         println("1. Jugar")
@@ -50,6 +92,8 @@ class Menu {
 
     fun jugar() {
         println("¡Bienvenido a la función Jugar!")
+        val carton = CartonBingoAleatorio()
+        carton.mostrarCarton()
         val simulador = Bombo()
         simulador.bombo()
     }
