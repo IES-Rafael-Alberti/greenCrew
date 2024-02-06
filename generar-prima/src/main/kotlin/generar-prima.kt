d29d364d23d5eea4ff1e323be3fb9643541242c8
JesusFlorB <146924987+JesusFlorB@users.noreply.github.com>
2024-02-06 18:05:32 +0100

    Base para trabajar en la PRIMA

---
+++ generar-prima/src/main/kotlin/generar-prima.kt
@@ -0,0 +1,19 @@
+import kotlin.random.Random
+
+class Prima {
+    var numeroPrima: Int = 0
+
+    fun generarNumeroPrima() {
+        numeroPrima = Random.nextInt(15, 51) // Se genera un número al azar para la prima del 15 al 50.
+    }
+
+    fun anunciarNumeroPrima() {
+        println("El número de prima para esta partida es el $numeroPrima. ¡Buena suerte!")
+    }
+}
+
+fun main() {
+    val prima = Prima()
+    prima.generarNumeroPrima()
+    prima.anunciarNumeroPrima()
+}
